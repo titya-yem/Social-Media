df63669a8ef7684d25b71c83be28c62a5f72806a
Titya Yem <thitya.yem.photo@gmail.com>
2026-01-05 20:53:24 -0500

    Feat: Run both app together (concurrently)

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import concurrently from "concurrently";
+
+// use concurrently to run multiple commands in parallel
+concurrently([
+    {
+        name: 'server',
+        command: 'bun run dev',
+        cwd: 'packages/server',
+        prefixColor: 'green',
+    },
+    {
+        name: 'client',
+        command: 'bun run dev',
+        cwd: 'packages/client',
+        prefixColor: 'cyan',
+    }
+])
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -9,6 +9,10 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Hello, Lamdouy !");
 });
 
+app.get("/api/hello", (req: Request, res: Response) => {
+    res.json({ message: "Hi, lamdouy user! "});
+});
+
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`)
