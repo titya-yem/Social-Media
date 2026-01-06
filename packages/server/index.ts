20dcdc09d708b746226135bc1f78fce334fe7223
Titya Yem <thitya.yem.photo@gmail.com>
2026-01-05 20:41:36 -0500

    Create frontend or client

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import type { Request, Response } from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Lamdouy !");
+    res.send("Hello, Lamdouy !");
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`)
 });
