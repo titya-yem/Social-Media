2d820a4c838553f2c58514df22b92df5adc9b92a
Titya Yem <thitya.yem.photo@gmail.com>
2026-01-06 21:39:19 -0500

    Feat: prettier

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
-import concurrently from "concurrently";
+import concurrently from 'concurrently';
 
 // use concurrently to run multiple commands in parallel
 concurrently([
-    {
-        name: 'server',
-        command: 'bun run dev',
-        cwd: 'packages/server',
-        prefixColor: 'green',
-    },
-    {
-        name: 'client',
-        command: 'bun run dev',
-        cwd: 'packages/client',
-        prefixColor: 'cyan',
-    }
-])
+  {
+    name: 'server',
+    command: 'bun run dev',
+    cwd: 'packages/server',
+    prefixColor: 'green',
+  },
+  {
+    name: 'client',
+    command: 'bun run dev',
+    cwd: 'packages/client',
+    prefixColor: 'cyan',
+  },
+]);
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,19 +1,19 @@
-import express from "express";
-import type { Request, Response } from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello, Lamdouy !");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, Lamdouy !');
 });
 
-app.get("/api/hello", (req: Request, res: Response) => {
-    res.json({ message: "Hi, lamdouy user! "});
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({ message: 'Hi, lamdouy user! ' });
 });
 
 const port = process.env.PORT;
 app.listen(port, () => {
-    console.log(`Server is running on port http://localhost:${port}`)
+  console.log(`Server is running on port http://localhost:${port}`);
 });
